9140ce00d053ea6b7a92cf826066b2962f8def4a
yayakame <83417264+yayakame@users.noreply.github.com>
2023-05-12 13:37:18 -0300

    implement SinglyLinkedNode

---
+++ algorithms-and-data-structures-rust/src/data_structures/singly_linked_node.rs
@@ -0,0 +1,13 @@
+pub struct SinglyLinkedNode<T>
+{
+    pub data: T,
+    pub next: Option<Box<SinglyLinkedNode<T>>>
+}
+
+impl<T> SinglyLinkedNode<T>
+{
+    pub fn new(data: T) -> SinglyLinkedNode<T>
+    {
+        SinglyLinkedNode { data, next: None }
+    }
+}
