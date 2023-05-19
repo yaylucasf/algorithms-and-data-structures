cd6330b77173c159325069962a9471be8f41c1e6
yayakame <83417264+yayakame@users.noreply.github.com>
2023-05-19 18:45:45 -0300

    implement `StackIterator`, `StackMutIterator` and `StackOwnedIterator`, and also `IntoInterator` for `Stack`

--- algorithms-and-data-structures-rust/src/data_structures/stack.rs
+++ algorithms-and-data-structures-rust/src/data_structures/stack.rs
@@ -7,7 +7,7 @@ pub struct Stack<T>
     size: usize,
 }
 
-impl<T> Stack<T>
+impl<'a, T> Stack<T>
 where
     T: Clone,
 {
@@ -34,29 +34,43 @@ where
 
     pub fn pop(&mut self) -> Option<T>
     {
-        if let Some(mut top_node) = self.top.take()
-        {
+        self.top.take().map(|mut top_node| {
             let ret_data = top_node.data.clone();
             self.top = top_node.next.take().map(|node| *node);
             self.size -= 1;
 
-            Some(ret_data)
-        }
-        else
-        {
-            None
-        }
+            ret_data
+        })
     }
 
     pub fn peek(&self) -> Option<T>
     {
-        if let Some(top_node) = &self.top
-        {
-            Some(unsafe { std::ptr::read(&top_node.data) })
+        self.top.as_ref().map(|top_node| {
+            unsafe { std::ptr::read(&top_node.data) }
+        })
+    }
+
+    pub fn get_top(&self) -> Option<&SinglyLinkedNode<T>>
+    {
+        self.top.as_ref()
+    }
+
+    pub fn get_top_mut(&mut self) -> Option<&mut SinglyLinkedNode<T>>
+    {
+        self.top.as_mut()
+    }
+
+    pub fn iter(&'a self) -> StackIterator<'a, T>
+    {
+        StackIterator {
+            next: self.top.as_ref(),
         }
-        else
-        {
-            None
+    }
+
+    pub fn iter_mut(&'a mut self) -> StackMutIterator<'a, T>
+    {
+        StackMutIterator {
+            next: self.top.as_mut(),
         }
     }
 
@@ -68,5 +82,70 @@ where
     pub fn get_size(&self) -> usize
     {
         self.size
+    }
+}
+
+pub struct StackIterator<'a, T>
+{
+    next: Option<&'a SinglyLinkedNode<T>>,
+}
+
+impl<'a, T> Iterator for StackIterator<'a, T>
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.data
+        })
+    }
+}
+
+pub struct StackMutIterator<'a, T>
+{
+    next: Option<&'a mut SinglyLinkedNode<T>>,
+}
+
+impl<'a, T> Iterator for StackMutIterator<'a, T>
+{
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.data
+        })
+    }
+}
+
+pub struct StackOwnedIterator<T>
+{
+    next: Option<SinglyLinkedNode<T>>,
+}
+
+impl<T> Iterator for StackOwnedIterator<T>
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        self.next.take().map(|node| {
+            self.next = node.next.map(|node| *node);
+            node.data
+        })
+    }
+}
+
+impl<T> IntoIterator for Stack<T>
+{
+    type Item = T;
+    type IntoIter = StackOwnedIterator<T>;
+
+    fn into_iter(self) -> Self::IntoIter
+    {
+        StackOwnedIterator { next: self.top }
     }
 }
